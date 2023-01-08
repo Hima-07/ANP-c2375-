cc2d38c8ffe37b64e7d4a8cfa90bee6e0ad482f5
Hima-07 <110650745+Hima-07@users.noreply.github.com>
2023-01-08 15:49:02 +0530

    NA

--- SpringProject-1/src/main/java/com/anudip/projt1/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.anudip.projt1.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.anudip.projt1.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
-
-}
